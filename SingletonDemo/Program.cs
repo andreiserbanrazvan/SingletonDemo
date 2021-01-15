d7b0751a2c07be00800a60f1db52691a21e4124a
Andrei Serban <andrei.serban@ciel.ro>
2021-01-15 17:11:39 +0200

    Add project files.

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
